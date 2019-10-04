086844c6d2259bf285cd5591f24890ff2f6c3e4a
Akim Kingori <akim.king12@gmail.com>
2019-10-04 11:55:49 +0300

    added location request feature
    the main activity has been modified to reply with a location link to the sender of a message with the word "Test" (ignores case).

--- app/src/main/java/com/abdev/offlinephonefinder/MainActivity.java
+++ app/src/main/java/com/abdev/offlinephonefinder/MainActivity.java
@@ -1,11 +1,20 @@
 package com.abdev.offlinephonefinder;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -13,10 +22,13 @@ import android.widget.ListView;
 import android.database.Cursor;
 import android.widget.Toast;
 import java.util.ArrayList;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
 
     //Button signIn;
@@ -32,11 +44,24 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> ListViewClickItemArray = new ArrayList<String>();
     String TempHolder;
+    SmsManager smsManager = SmsManager.getDefault();
+    Intent j;
+    String Sender;
+    String messageBody;
+    protected LocationManager locationManager;
+    protected boolean gps_enabled, network_enabled;
 
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkAndRequestPermissions();
         //signIn = findViewById(R.id.next);
 //        signIn.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -75,6 +100,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        checkIntent();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // getting GPS status
+        gps_enabled = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // getting network status
+        network_enabled = locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (messageBody != null){
+            if(messageBody.equalsIgnoreCase("Test")){
+                if (network_enabled) {
+
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                }
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+
+
+            }
+        }
+
     }
 
     @Override
@@ -111,5 +165,79 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.e("location change","location");
+        Intent i = getIntent();
+        double lat = location.getLatitude();
+        String sender = getIntent().getStringExtra("sender");
+        String message =getIntent().getStringExtra("message");
+        if(message.equalsIgnoreCase("Test"))
+            smsManager.sendTextMessage(sender, null, "http://maps.google.com/maps?q="+location.getLatitude()+","+location.getLongitude(), null, null);
+        else if(message.equalsIgnoreCase("ringer")){
+            AudioManager audio_mngr = (AudioManager) getBaseContext().getSystemService(Context.AUDIO_SERVICE);
+            audio_mngr .setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+        }
+    }
 
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    private void checkIntent() {
+        j = getIntent();
+        String sender = getIntent().getStringExtra("sender");
+        messageBody = getIntent().getStringExtra("message");
+        Sender =sender;
+
+
+    }
+
+    private void checkAndRequestPermissions() {
+        int permissionSendMessage = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS);
+
+        int receiveSMS = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECEIVE_SMS);
+
+
+        int coarseLocation = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+        int fineLocation = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        if (receiveSMS != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.RECEIVE_SMS);
+        }
+
+        if (permissionSendMessage != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.SEND_SMS);
+        }
+        if (coarseLocation != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if (fineLocation != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),
+                    101);
+            //return false;
+        }
+        //return true;
+    }
 }
